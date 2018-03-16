08bcb595b482d96aca96d2d969c03fc246670d10
Balaji <balaji030698@gmail.com>
2018-03-16 23:00:38 +0530

    added init file

---
+++ init.rb
@@ -0,0 +1,6 @@
+# # storing the root location in a constant. 
+# APP_ROOT = File.dirname(__FILE__) 
+# require 'sinatra' 
+# require 'json' 
+# # loading the bot class require 
+# File.join(APP_ROOT,"lib","twitter_bot")
