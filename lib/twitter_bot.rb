1978f129114808f13160c048462f6e54c5d3a0eb
Balaji <balaji030698@gmail.com>
2018-03-21 19:27:07 +0530

    edited http to https in url, few edits in twitter.rb file

--- lib/twitter_bot.rb
+++ lib/twitter_bot.rb
@@ -12,21 +12,20 @@ class TwitterBot
 		$client.user do |obj|
 			case obj
 			when Twitter::Tweet		
-				puts "status id #{obj.id}"
-				puts "text #{obj.text}"
-				puts "User Id #{obj.user.id}"
-				puts "User Name #{obj.user.screen_name}"
-				puts "user_mentions #{obj.user_mentions[0].screen_name.to_s}"
+				puts "status id: #{obj.id}"
+				puts "text: #{obj.text}"
+				puts "User Id: #{obj.user.id}"
+				puts "User Name: #{obj.user.screen_name}"
+				puts "user_mentions: #{obj.user_mentions[0].screen_name}"
 				# Below code triggers when user mention occured
-				if obj.user_mentions? && obj.user_mentions[0].screen_name.to_s == "giphy_emoji"
-					puts obj.user_mentions.to_s
+				if obj.user_mentions? && obj.user_mentions[0].screen_name == "giphy_emoji"
+					puts obj.user_mentions
 					text = obj.text.gsub("@giphy_emoji","")
-					text = text.gsub("_","+")
 					text = text.strip
-					puts "#{text}"
+					puts "Text: #{text}"
 					begin
 						emoji_text = Emoji.find_by_unicode("#{text}").name
-						puts "#{emoji_text}"
+						puts "Emoji text: #{emoji_text}"
 						giphy_response = HTTParty.get(GIPHY_URL + emoji_text + GIPHY_API_KEY_URL)
 						giphy_response = giphy_response.body
 						giphy_parsed_response = JSON.parse(giphy_response)
--- utils.rb
+++ utils.rb
@@ -1,4 +1,4 @@
 require 'dotenv/load'
 # Giphy api urls
-GIPHY_URL = "http://api.giphy.com/v1/gifs/search?q="
+GIPHY_URL = "https://api.giphy.com/v1/gifs/search?q="
 GIPHY_API_KEY_URL = "&api_key=" + ENV["GIPHY_API_KEY"] +"&limit=1"
